f1f3d4b0ef8fbe9ff83758465fb1d8c653d91f4d
João Melo <31737800+jhonas8@users.noreply.github.com>
2022-10-04 17:19:30 -0300

    Delete target directory

--- target/app/button.js
+++
@@ -1,7 +0,0 @@
-// Compiled by ClojureScript 1.10.844 {:target :nodejs, :nodejs-rt false}
-goog.provide('app.button');
-goog.require('cljs.core');
-goog.require('reagent.core');
-goog.require('app.ui');
-
-//# sourceMappingURL=button.js.map
--- target/app/helpers/calculator_button.js
+++
@@ -1,7 +0,0 @@
-// Compiled by ClojureScript 1.10.844 {:target :nodejs, :nodejs-rt false}
-goog.provide('app.helpers.calculator_button');
-goog.require('cljs.core');
-goog.require('reagent.core');
-app.helpers.calculator_button.node$module$react_native = require('react-native');
-
-//# sourceMappingURL=calculator_button.js.map
